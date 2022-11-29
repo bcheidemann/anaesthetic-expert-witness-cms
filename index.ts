984af2d2c8d24977172a49c611a1a4e8f7b3b570
Ben Heidemann <ben@heidemann.co.uk>
2022-11-29 22:39:49 +0000

    fit typo

--- index.ts
+++ index.ts
@@ -3,6 +3,6 @@ import { footerSection } from "./schemas/footerSection";
 import { image } from "./schemas/image";
 import { route } from "./schemas/route";
 
-export type FoooterSection = s.infer<typeof footerSection>;
+export type FooterSection = s.infer<typeof footerSection>;
 export type Image = s.infer<typeof image>;
 export type Route = s.infer<typeof route>;
