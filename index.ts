af0582c612f5f758f37986b09e866c667ff95d94
Ben Heidemann <ben@heidemann.co.uk>
2022-12-01 23:28:13 +0000

    export nav bar item

--- index.ts
+++ index.ts
@@ -2,7 +2,9 @@ import { s } from "sanity-typed-schema-builder";
 import { footerSection } from "./schemas/footerSection";
 import { image } from "./schemas/image";
 import { route } from "./schemas/route";
+import { navBarItem } from "./schemas/navBarItem";
 
 export type FooterSection = s.infer<typeof footerSection>;
 export type Image = s.infer<typeof image>;
 export type Route = s.infer<typeof route>;
+export type NavBarItem = s.infer<typeof navBarItem>;
